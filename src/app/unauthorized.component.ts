7ecc385d8c587f785fc65678ab4da58540ea5434
Lagisetty <619735@bah.com>
2022-03-23 18:09:19 -0400

    poc_1

---
+++ src/app/unauthorized.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'app-root',
+  template: `<h2>Un authroized !!!</h2>`
+})
+export class UnauthorizedComponent {
+             
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  oauthClientId: '0oajs1ldhWXNQvwp2696',
+  oauthIssuerUrl: 'https://trial-2042258.okta.com/oauth2/ausn089mrnUkPCaQ9696',
+  oauthLoginUrl: 'https://trial-2042258.okta.com/oauth2/ausn089mrnUkPCaQ9696/v1/authorize',
+  oauthTokenUrl: 'https://trial-2042258.okta.com/oauth2/ausn089mrnUkPCaQ9696/v1/token',
+  oauthUserInfoUrl: 'https://trial-2042258.okta.com/oauth2/ausn089mrnUkPCaQ9696/v1/userinfo',
+  oauthCallbackUrl: 'http://localhost:4200/dashboard',
+  oauthPostLogoutUrl: 'https://trial-2042258.okta.com/login/signout'
+}; 
+ 
+/*
+https://trial-2042258.okta.com/oauth2/default	
+ausn089mrnUkPCaQ9696
+
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
